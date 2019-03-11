3c10b07ba9c77e5e9fa28a64eb6a9da73a589597
Miklós Árpád <ethax86@gmail.com>
2019-03-11 22:27:34 +0100

    Tweaked the unit tests of 'boris_corrector'.

--- boris-corrector/test/test_boris_corrector.cpp
+++ boris-corrector/test/test_boris_corrector.cpp
@@ -6,13 +6,10 @@
 using namespace Catch;
 using namespace diplomamunka;
 
-using string_pair = std::pair<std::string, std::string>;
-
 TEST_CASE("Append temporary file prefix")
 {
     const fs::path path = "C:\\Windows\\System32\\cmd.exe";
-
-    REQUIRE_THAT(add_temp_prefix(path).string(), Equals("C:\\Windows\\System32\\~$cmd.exe"));
+    REQUIRE(add_temp_prefix(path) == "C:\\Windows\\System32\\~$cmd.exe");
 }
 
 TEST_CASE("Copy only BORIS-specific files")
@@ -26,8 +23,8 @@ TEST_CASE("Copy only BORIS-specific files")
         "E:\\Projects\\BORIS Projects\\documentation\\Project Specification.docx",
         "E:\\Projects\\BORIS Projects\\media\\demo.mp4"
     };
-    std::vector<fs::path> boris_only;
 
+    std::vector<fs::path> boris_only;
     std::copy_if(mixed.begin(), mixed.end(), std::back_inserter(boris_only), is_boris_file);
 
     REQUIRE_THAT(boris_only, Equals<fs::path>({
@@ -55,31 +52,18 @@ TEST_CASE("Replace paths in string to their counterparts.")
 
     SECTION("The whole line is a path")
     {
-        auto [value, expected] = GENERATE(values<string_pair>({
-            { 
-                "D:\\Studies\\Diplomamunka\\Emulated System\\Emulation\\media\\logo.bmp",
-                "E:\\Diplomamunka\\BORIS\\Emulation\\media\\logo.bmp"
-            },
-            {
-                "D:\\Studies\\Diplomamunka\\Emulated System\\Emulation\\main.bsy",
-                "E:\\Diplomamunka\\BORIS\\Emulation\\main.bsy"
-            },
-            {
-                "D:\\Studies\\Diplomamunka\\Emulated System\\Emulation\\animation\\anim.fab",
-                "E:\\Diplomamunka\\BORIS\\Emulation\\animation\\anim.fab"
-            },
-            {
-                "D:\\Studies\\Diplomamunka\\Emulated System\\Emulation\\lib\\engine.sbl",
-                "E:\\Diplomamunka\\BORIS\\Emulation\\lib\\engine.sbl"
-            }
+        const auto suffix = GENERATE(values<std::string>({
+            "media\\logo.bmp", "main.bsy", "animation\\anim.fab", "lib\\engine.sbl"
         }));
-        
-        REQUIRE_THAT(corrector.correct_paths(value, new_paths), Equals(expected));
+
+        const auto text = "D:\\Studies\\Diplomamunka\\Emulated System\\Emulation\\" + suffix;
+        const auto result = corrector.correct_paths(text, new_paths);
+        REQUIRE_THAT(result, Equals("E:\\Diplomamunka\\BORIS\\Emulation\\" + suffix));
     }
 
     SECTION("Text mixed with paths")
     {
-        std::string text {
+        const std::string text {
             "BMPSEQ\t0\t16777215\t2\t-1\t0\t0\tCell 2: Belt\t425\t390\t375\t80\t2\t5\t"
             "D:\\Studies\\Diplomamunka\\Emulated System\\Emulation\\animation\\belt_01.bmp\t"
             "D:\\Studies\\Diplomamunka\\Emulated System\\Emulation\\animation\\belt_02.bmp\t"
@@ -89,9 +73,7 @@ TEST_CASE("Replace paths in string to their counterparts.")
             "*.bmp\t*.bmp\t*.bmp\t*.bmp\t*.bmp\t0\t0\t5\t0\tC:\\Windows\\System32\\x64.bmp"
         };
         
-        text = corrector.correct_paths(text, new_paths);
-
-        REQUIRE_THAT(text, Equals({
+        REQUIRE_THAT(corrector.correct_paths(text, new_paths), Equals({
             "BMPSEQ\t0\t16777215\t2\t-1\t0\t0\tCell 2: Belt\t425\t390\t375\t80\t2\t5\t"
             "E:\\Diplomamunka\\BORIS\\Emulation\\animation\\belt_01.bmp\t"
             "E:\\Diplomamunka\\BORIS\\Emulation\\animation\\belt_02.bmp\t"
